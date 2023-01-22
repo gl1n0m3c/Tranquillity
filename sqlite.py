078c2988129301dfaad5f1105173b3e4c500835c
pavel <pavel.krasavchik06@gmail.com>
2023-01-22 15:15:58 +0300

    запись в таблицы

--- sqlite.py
+++ sqlite.py
@@ -6,38 +6,100 @@ cur = conn.cursor()
 #создание таблиц time, air, ground
 cur.execute("""CREATE TABLE IF NOT EXISTS time(
    time TEXT,
-   type INT);
+   type TEXT);
 """)
 conn.commit()
 cur.execute("""CREATE TABLE IF NOT EXISTS air(
-   type INT,
-   result INT,
-   id INT,
-   temperature REAL,
-   humidity REAL,
+   type TEXT,
+   result TEXT,
+   id TEXT,
+   temperature TEXT,
+   humidity TEXT,
    time TEXT);
 """)
 conn.commit()
 cur.execute("""CREATE TABLE IF NOT EXISTS ground(
-   type INT,
-   result INT,
-   id INT,  
-   humidity REAL,
+   type TEXT,
+   result TEXT,
+   id TEXT,  
+   humidity TEXT,
    time TEXT);
 """)
 conn.commit()
 
 #работа с кортежами для занесения данных в таблицу
-var = {'timeAIR': '2023-01-22 13:04:10',
-       'timeGROUND': '2023-01-22 13:04:10',
+var = {'timeAIR': '2023-01-22 13:24:07',
+       'timeGROUND': '2023-01-22 13:24:08',
        'data': {
-           'air': [{'result': True, 'id': 1, 'temperature': 28.35, 'humidity': 74.54},
-                   {'result': True, 'id': 2, 'temperature': 31.96, 'humidity': 64.07},
-                   {'result': True, 'id': 3, 'temperature': 28.92, 'humidity': 73.09},
-                   {'result': True, 'id': 4, 'temperature': 29.49, 'humidity': 62.76}],
-           'ground': [{'result': True, 'id': 1, 'humidity': 70.81},
-                      {'result': True, 'id': 2, 'humidity': 68.25},
-                      {'result': True, 'id': 3, 'humidity': 65.44},
-                      {'result': True, 'id': 4, 'humidity': 75.3},
-                      {'result': True, 'id': 5, 'humidity': 72.79},
-                      {'result': True, 'id': 6, 'humidity': 70.41}]}}
+           'air': [{
+               'result': False, 'id': 1},
+               {'result': True, 'id': 2, 'temperature': 29.2, 'humidity': 63.43},
+               {'result': False, 'id': 3},
+               {'result': False, 'id': 4}],
+           'ground': [{
+               'result': False, 'id': 1},
+               {'result': True, 'id': 2, 'humidity': 66.4},
+               {'result': False, 'id': 3},
+               {'result': True, 'id': 4, 'humidity': 70.94},
+               {'result': False, 'id': 5},
+               {'result': False, 'id': 6}]}}
+#запись в таблицу air
+
+for i in range(0,4):
+    if (str(var['data']['air'][i]['result']) == 'True'):
+        type = '1'
+        result = str(var['data']['air'][i]['result'])
+        id = str(var['data']['air'][i]['id'])
+        temperature = str(var['data']['air'][i]['temperature'])
+        humidity = str(var['data']['air'][i]['humidity'])
+        time = '-'
+        aird = (type,result,id,temperature,humidity,time)
+        cur.execute("INSERT INTO air VALUES(?, ?, ?, ?, ?, ?);", aird)
+        conn.commit()
+        aird = ()
+    else:
+        type = '1'
+        result = str(var['data']['air'][i]['result'])
+        id = str(var['data']['air'][i]['id'])
+        temperature = 'NULL'
+        humidity = 'NULL'
+        time = '-'
+        aird = (type,result,id,temperature,humidity,time)
+        cur.execute("INSERT INTO air VALUES(?, ?, ?, ?, ?, ?);", aird)
+        conn.commit()
+        aird = ()
+
+#запись в таблицу ground
+for i in range(0,6):
+    if (str(var['data']['ground'][i]['result']) == 'True'):
+        type = '1'
+        result = str(var['data']['ground'][i]['result'])
+        id = str(var['data']['ground'][i]['id'])
+        humidity = str(var['data']['ground'][i]['humidity'])
+        time = '-'
+        grd = (type,result,id,humidity,time)
+        cur.execute("INSERT INTO ground VALUES(?, ?, ?, ?, ?);", grd)
+        conn.commit()
+        grd = ()
+    else:
+        type = '1'
+        result = str(var['data']['ground'][i]['result'])
+        id = str(var['data']['ground'][i]['id'])
+        humidity = 'NULL'
+        time = '-'
+        grd = (type,result,id,humidity,time)
+        cur.execute("INSERT INTO ground VALUES(?, ?, ?, ?, ?);", grd)
+        conn.commit()
+        grd = ()
+
+
+cur.execute("SELECT * FROM air;")
+all_results = cur.fetchall()
+print(all_results)
+cur.execute("SELECT * FROM ground;")
+all_results1 = cur.fetchall()
+print(all_results1)
+conn.close()
+
+
+
