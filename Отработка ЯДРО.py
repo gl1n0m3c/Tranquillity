3fe68f26104cda8b6da02ff2686325463a967bf3
Коля <nperewerzew@gmail.com>
2023-01-23 17:29:22 +0300

    НАКОНЕЦ-ТО РАБОТАЕТ PATCH!!! (Я ГЕНИЙ!!)

--- Отработка ЯДРО.py
+++ Отработка ЯДРО.py
@@ -1,5 +1,10 @@
+import json
 import requests
 
 URL = 'https://dt.miet.ru/ppo_it/api/total_hum'
-response = requests.patch(URL)
-print(response.json())
+token = {"X-Auth-Token": "97UviE"}
+
+r = requests.patch('https://dt.miet.ru/ppo_it/api/watering', params={"id": 1, "state": 0})
+print(r.status_code)
+print(r.headers)
+print(r.json())
