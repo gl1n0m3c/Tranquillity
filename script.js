d624d9c48fc8f73779d2bae0e97d1d8e1868e88a
Андрей Кашин <nubov.930@gmail.com>
2023-01-29 21:52:06 +0300

    update

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const getResourse = async(url) => {
     const response = await fetch(url);
-    console.log(response)
+    
+    return await response.json();
 };
 
 
-getResourse('https://jsonplaceholder.typicode.com/todos/1')
+getResourse('https://my-json-server.typicode.com/valeriy-egorov/FakeOnlineRESTserver/options').then((data) => console.log(data))
