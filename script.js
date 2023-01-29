961368fcfa96a93c7807c1f0ce80bc07fbeef682
Андрей Кашин <nubov.930@gmail.com>
2023-01-29 20:30:11 +0300

    update 2

--- script.js
+++ script.js
@@ -1,34 +1,7 @@
-document.addEventListener("DOMContentLoaded", () => {
+const getResourse = async(url) => {
+    const response = await fetch(url);
+    console.log(response)
+};
 
-    const Send = (formData) => {
-        fetch("mail.php", { // файл-обработчик
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json", // отправляемые данные
-            },
-            body: JSON.stringify(formData)
-        })
-            .then(response => alert("Сообщение отправлено"))
-            .catch(error => console.error(error))
-    };
 
-    if (document.getElementsByTagName("form")) {
-        const forms = document.getElementsByTagName("form");
-
-        for (let i = 0; i < forms.length; i++) {
-            forms[i].addEventListener("submit", function (e) {
-                e.preventDefault();
-
-                let formData = new FormData(this);
-                formData = Object.fromEntries(formData);
-
-                Send(formData)
-                    .then((response) => {
-                        console.log(response);
-                    })
-                    .catch((err) => console.error(err))
-            });
-        };
-    }
-});
-
+getResourse('https://jsonplaceholder.typicode.com/todos/1')
