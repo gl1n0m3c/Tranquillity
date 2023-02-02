31fc58d78f929f2c7a15119a07422fbdbc57fb1a
Андрей Кашин <nubov.930@gmail.com>
2023-02-02 20:34:00 +0300

    1

--- script.js
+++ script.js
@@ -6,19 +6,88 @@ const getResourse = async(url) => {
 
 getResourse('https://my-json-server.typicode.com/valeriy-egorov/FakeOnlineRESTserver/options').then((data) => console.log(data))
 
+async function get1(){
+    
+        
+    
+    };
 
-function sendForm(e){
-     
-    // получаем значение поля key
-    var keyBox = document.search.key;
-    var val = keyBox.value;
-    if(val.length>5){
-        alert("Недопустимая длина строки");
-        e.preventDefault();
-    }   
-    else
-        alert("Отправка разрешена");
-}
- 
-var sendButton = document.search.send;
-sendButton.addEventListener("click", sendForm);
+
+
+async function get(num){
+    if (num==1){
+    
+        const ajaxSend = async (formData) => {
+            const response = await fetch("http://localhost:8000//save_temperature/", {
+                method: "GET",
+            });
+        };
+    
+        if (document.querySelector("form")) {
+            const forms = document.querySelectorAll("form");
+    
+            forms.forEach(form => {
+                form.addEventListener("submit", function (e) {
+                    e.preventDefault();
+                    const formData = new FormData(this);
+    
+                    ajaxSend(formData)
+                        .then((response) => {
+                            console.log('hi');
+                            form.reset(); // очищаем поля формы
+                        })
+                });
+            });
+        }
+    }   if (num==2){
+                const ajaxSend = async (formData) => {
+                const response = await fetch("http://localhost:8000/save_air_humidity/", {
+                    method: "GET",
+                });
+            };
+
+            if (document.querySelector("form")) {
+                const forms = document.querySelectorAll("form");
+
+                forms.forEach(form => {
+                    form.addEventListener("submit", function (e) {
+                        e.preventDefault();
+                        const formData = new FormData(this);
+
+                        ajaxSend(formData)
+                            .then((response) => {
+                                console.log('hello');
+                                form.reset(); // очищаем поля формы
+
+                            })
+                    });
+                });
+            }
+        } if(num==3){
+
+
+            
+                const ajaxSend = async (formData) => {
+                    const response = await fetch("http://localhost:8000//save_ground_humidity/", {
+                        method: "GET",
+                    });
+                };
+                
+                if (document.querySelector("form")) {
+                    const forms = document.querySelectorAll("form");
+                
+                    forms.forEach(form => {
+                        form.addEventListener("submit", function (e) {
+                            e.preventDefault();
+                            const formData = new FormData(this);
+                
+                            ajaxSend(formData)
+                                .then((response) => {
+                                    console.log('gutentag');
+                                    form.reset(); // очищаем поля формы
+                                })
+                        });
+                    });
+                }
+            
+        }};
