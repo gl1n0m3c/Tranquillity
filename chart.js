68bebff8196e542630f5fde1fa2cfa2eade90c03
Hunterito <120974987+Hunterito@users.noreply.github.com>
2023-02-08 22:19:17 +0300

    Почти готовый код Chart'a для нашего сервера

--- chart.js
+++ chart.js
@@ -1,49 +1,24 @@
-const URL = 'https://my-json-server.typicode.com/valeriy-egorov/FakeOnlineRESTserver/DATA';
-var Chart1;						//Экземпляр объекта Chart
-var Sensor;						//Массив данных полученных от сервера
-const ColorLeft = 'rgb(255, 100, 100)';			//Настройки графика
-const ColorRight = 'rgb(100, 100, 255)';		//Настройки графика
-moment.locale('ru');					//Настройки графика. Переключение языка на русский для строк с датой
-
-var Index = 0;						//Для демонстрации
+const URL = "http://localhost:8000/give_data/30min";
+var Chart1;							
+moment.locale('ru');
 
 //Обработка нажатий
 ChartRefresh1.onclick = function() {
     DataFetch(URL);
     };
-  
-//Функция получения и отображения данных с сервера
+
 async function DataFetch(strURL) {
     const Requ = new Request(strURL);
     const Resp = await fetch(Requ);
-    const Data = await Resp.json();
-  
-    //Для демонстрации
-    Sensor = Data[Index % 2];				//Для демонстрации изменяющихся данных
-    Index += 1;						//Для демонстрации
-    console.log(Sensor);					//Для отладки
-
-//Манипуляции с тэгами HTML
-//  const TagHeader = document.querySelector('header');	//Запрашиваем элемент <header> из документа
-//  const DataName = document.createElement('div');	//Создаём элемент DataName в блоке div
-//  TagHeader.appendChild(DataName);			//Добавляем в элемент <header> подготовленный элемент DataName
-//  const Section = document.querySelector('section');	//Запрашиваем элемент <selection> из документа
-//  for (const Rec of Sensor.air) {			//Цикл по всем записям Sensor.air
-//      const Article = document.createElement('article');	//Создаём элемент article
-//      const Para = document.createElement('span');	//Создаём элемент span
-//      Para.textContent = `Дата: ${Rec.dt}, t1=${Rec.t1}`;	//Заполняем свойства элемента
-//      Article.appendChild(Para);				//Добавляем в элемент Article подготовленный элемент Para
-//      Section.appendChild(Article);			//Добавляем в элемент <section> подготовленный элемент Article
-//      }
+    var Sensor = await Resp.json();
     
-  //Манипуляции с графиком
-    if (Chart1 != undefined) {				//Экземпляр объекта Chart существует?
+    if (Chart1 != undefined) {				
         Chart1.data.datasets.forEach(dataset => {
-            dataset.data = Sensor.air;			//Обновить данные
+            dataset.data = Sensor.air;			
         });
-        Chart1.update("resize");				//Обновить график
+        Chart1.update("resize");			
         } else {
-            Chart1 = new Chart('chart1', {			//Создаём один раз и потом используем
+            Chart1 = new Chart('chart1', {			
             type: 'line',
             data: {
                 datasets: [
@@ -51,11 +26,11 @@ async function DataFetch(strURL) {
                 label: 'Первый',
                 data: Sensor.air,
                 yAxisID: 'y',
-                parsing: {xAxisKey: "dt", yAxisKey: "t1"},	//задание соответствия полей данных к осям
+                parsing: {xAxisKey: "dt", yAxisKey: "t1"},	
                 showLine: true,
                 hidden: false,
                 lineTension: 0.3,
-                borderColor: ColorLeft},
+                color: "#FF0000"},
             {
                 label: 'Второй',
                 data: Sensor.air,
@@ -64,8 +39,8 @@ async function DataFetch(strURL) {
                 showLine: true,
                 hidden: true,
                 lineTension: 0.3,
-                color: "#00FF00",
-                borderColor: ColorRight},
+                color: "#00FF00"
+            },
             {
                 label: 'Третий',
                 data: Sensor.air,
@@ -74,8 +49,8 @@ async function DataFetch(strURL) {
                 showLine: true,
                 hidden: true,
                 lineTension: 0.3,
-                color: "#0000FF",
-                borderColor: ColorLeft},
+                color: "#0000FF"
+            },
             {
                 label: 'Четвертый',
                 data: Sensor.air,
@@ -84,16 +59,16 @@ async function DataFetch(strURL) {
                 showLine: true,
                 hidden: true,
                 lineTension: 0.3,
-                color: "#FFFFFF",
-                borderColor: ColorRight}],
+                color: "#FF00FF"
+            }],
     },
         options: {
             locale: "ru-RU",
-            animation: false,
+            animation: true,
             plugins: {
         title: {
             display: true,
-            text: 'Датчики воздуха',
+            text: 'Датчики температуры',
           },
         legend: {
             labels: {
@@ -114,11 +89,11 @@ async function DataFetch(strURL) {
                 type: 'time',
 	    title: {
   	    display: true,
-  	    text: "Ось времени ",
+  	    text: "Время",
 	    },
             time: {
-	      isoWeekday: true,
-	      minUnit: 'second',
+	    isoWeekday: true,
+	    minUnit: 'minute',
                 displayFormats: {				
   	        millisecond: 'mm:ss.SSS',
   	        second: 'hh:mm:ss',
@@ -151,19 +126,19 @@ async function DataFetch(strURL) {
             display: true,
             position: 'left',
 	    border: {
-  	    color: ColorLeft,
+  	    color: "#000000",
 	    },
             ticks: {
-                color: ColorLeft,
+                color: "#000000",
 	    },
 	    title: {
   	    display: true,
-  	    color: ColorLeft,
+  	    color: "#000000",
 	        text: "Температура",
 	    },  
         grid: {
-            color: ColorRight,
+            color: "#000000",
             }
-          }}}});
+        }}}});
     }
   }
